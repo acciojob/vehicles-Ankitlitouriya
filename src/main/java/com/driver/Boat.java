b8142e2e92e65609908e403fa4e2a2c6af82c887
Ankit Litouriya <ankitlitouriya8719@gmail.com>
2023-02-11 21:46:41 +0530

    final

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,22 +1,17 @@
 package com.driver;
 
-public class Boat implements WaterVehicle{
+public class Boat implements WaterVehicle {
+    public String name;
+    public int capacity;
 
-    String name;
-    int capacity;
-
-    Boat(String name, int capacity)
-    {
-        this.name= name;
-        this.capacity= capacity;
+    public Boat(String name,int capacity){
+        this.name = name;
+        this.capacity = capacity;
     }
-    @Override
-    public String getVehicleName() {
+    public String getVehicleName(){
         return name;
     }
-
-    @Override
-    public int getVehicleCapacity() {
+    public int getVehicleCapacity(){
         return capacity;
     }
 }
