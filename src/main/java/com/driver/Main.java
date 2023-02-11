38509bd829c6434e03c66b9fb4986eabc121761e
Ankit Litouriya <ankitlitouriya8719@gmail.com>
2023-02-11 20:47:57 +0530

    first

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,18 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
+
         F1 mercedes = new F1("mercedes", false);
         mercedes.move(40, 0);
         mercedes.accelerate(20);
         mercedes.accelerate(-60);
+
+        Boat b = new Boat("lara",100);
+        System.out.println(b.getVehicleName());
+        System.out.println(b.getVehicleCapacity());
+
+
     }
+
+
 }
